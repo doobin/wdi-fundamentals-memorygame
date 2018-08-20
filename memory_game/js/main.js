e5cfaf2aeb2cbd9ff821cb7c2fc5f309dac075e8
Doo Bin Im <doobin.im@Doos-MBP.fios-router.home>
2018-08-20 14:00:02 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+var cardOne = "queen of diamonds";
+var cardTwo = "king of diamonds";
+var cardThree = "queen of hearts";
+var cardFour = "king of hearts";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
